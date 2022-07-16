8508cb2869289fc38c0f3095c1a608676f88ad1d
Ruslan <81380652+SomaSan2005@users.noreply.github.com>
2022-07-16 15:31:43 +0200

    Add files via upload

---
+++ DeitelBook/N2.24Pag65.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{ 
+    
+    int num; 
+
+    printf("inserire un numero: "); 
+    scanf("%d", &num);
+
+    if (num%2==0) 
+        printf("il numero inserito è pari"); 
+    else 
+        printf("il numero inserito è dispari"); 
+}
