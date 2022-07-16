efa58843d544be82039074babfe8e0a0da29f938
SomaSan2005 <ruslanm0502@gmail.com>
2022-07-16 18:41:46 +0200

    N3.18Pag107.c

--- DeitelBook Exercises/N3.18Pag107.c
+++ DeitelBook Exercises/N3.18Pag107.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
 
     float guadagno = 0;
     int percentuale = 0;
     float salary = 0;
+    char risposta = ' '; 
+    risposta = 'N';
+   
+    do
+    {
 
     printf("inserire valore del guadagno dellle vendite: ");
     scanf("%f", &guadagno);
     printf("inserire la percentuale: ");
     scanf("%d", &percentuale);
-
     salary = ((guadagno / 100) * percentuale) + 200;
-
     printf("Salary: %2.0f", salary);
-}
+    printf("\nvoi continare si o no ? S/N:  ");
+    scanf(" %c",&risposta);
+
+    }while (risposta=='S');
+    
