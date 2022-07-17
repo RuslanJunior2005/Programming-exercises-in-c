55edde9cf4286def9108875413c9d32d636282fb
SomaSan2005 <ruslanm0502@gmail.com>
2022-07-17 18:49:21 +0200

    DeitelBook Exercises

--- DeitelBook Exercises/N3.16Pag106.c
+++ DeitelBook Exercises/N3.16Pag106.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
+
 int main()
 {
-    float kilometri = 0; 
-    float  litri =0; 
-    float kilometriPerLitro = 0;
-    char risposta = ' '; 
-    risposta = 'N';
 
-     do
+    float guadagno = 0;
+    int percentuale = 0;
+    float salary = 0;
+    char risposta = ' '; 
+    risposta = 'S';
+   
+/*    do
     {
 
-    printf("\ninserisci i litri che hai messo nella vettura: ");
-    scanf("%f",&litri);
-
-    printf("\ninserisci i kelometri che hai percorso: ");
-    scanf("%f",&kilometri); 
-
-   kilometriPerLitro = (kilometri/litri);
-
-   printf("\nkilometri percorsi per litri: %f",kilometriPerLitro);
-
+    printf("inserire valore del guadagno dellle vendite: ");
+    scanf("%f", &guadagno);
+    printf("inserire la percentuale: ");
+    scanf("%d", &percentuale);
+    salary = ((guadagno / 100) * percentuale) + 200;
+    printf("Salary: %2.0f", salary);
     printf("\nvoi continare si o no ? S/N:  ");
     scanf(" %c",&risposta);
 
-    }
-    while (risposta=='S');
+    }while (risposta=='S');
+  */
 
-}
+ while( risposta == 'S' )
+ {
+    printf("inserire valore del guadagno dellle vendite: ");
+    scanf("%f", &guadagno);
+    printf("inserire la percentuale: ");
+    scanf("%d", &percentuale);
+    salary = ((guadagno / 100) * percentuale) + 200;
+    printf("Salary: %2.0f", salary);
+    printf("\nvoi continare si o no ? S/N:  ");
+    scanf(" %c",&risposta);
+ }
--- DeitelBook Exercises/N3.18Pag107.c
+++ DeitelBook Exercises/N3.18Pag107.c
@@ -7,9 +7,9 @@ int main()
     int percentuale = 0;
     float salary = 0;
     char risposta = ' '; 
-    risposta = 'N';
+    risposta = 'S';
    
-    do
+/*    do
     {
 
     printf("inserire valore del guadagno dellle vendite: ");
@@ -21,8 +21,21 @@ int main()
     printf("\nvoi continare si o no ? S/N:  ");
     scanf(" %c",&risposta);
 
-    }
-    while (risposta=='S');
+    }while (risposta=='S');
+  */
+
+ while( risposta == 'S' )
+ {
+    printf("inserire valore del guadagno dellle vendite: ");
+    scanf("%f", &guadagno);
+    printf("inserire la percentuale: ");
+    scanf("%d", &percentuale);
+    salary = ((guadagno / 100) * percentuale) + 200;
+    printf("Salary: %2.0f", salary);
+    printf("\nvoi continare si o no ? S/N:  ");
+    scanf(" %c",&risposta);
+ }
+
     
 
 
