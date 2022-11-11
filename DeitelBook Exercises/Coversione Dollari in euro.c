672062a067c608f41109ce133cb3f99f930c4d3b
SomaSan2005 <81380652+SomaSan2005@users.noreply.github.com>
2022-11-11 10:51:59 +0100

    Create Coversione Dollari in euro.c

---
+++ DeitelBook Exercises/Coversione Dollari in euro.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+  int dollaro = 0;
+  
+  int   risultato = 0;
+	 	
+	 	
+	 	 
+ printf("metti la valuta da convertire in da dollari a euro: ");
+	 	scanf("%d",&dollaro);
+	 	
+	risultato = (dollaro / 1.2745);  
+	 	
+	 printf("conversione e': ");
+	 		
+	 	printf("%d",risultato);
+	 	
+	 
+}
